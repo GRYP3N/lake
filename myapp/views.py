9486288ee70962f2535541f565c1dca2f908493c
NOBELIUM <nathan_garciaa@yahoo.com>
2026-03-04 05:21:38 +0000

    Add homepage view and URL mapping

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    # simple response; replace with a template later
+    return HttpResponse("<h1>Welcome to the Lake project!</h1>")
